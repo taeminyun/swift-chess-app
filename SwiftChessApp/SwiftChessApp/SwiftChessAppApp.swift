8e7a150e9c11190a5dbf9556e0348c1fb5355f1c
taehyeon.lee <taehyeon.lee@taehyeon-ui-MacBookPro.local>
2022-09-25 22:10:44 +0900

    프로젝트 시작

---
+++ SwiftChessApp/SwiftChessApp/SwiftChessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChessAppApp.swift
+//  SwiftChessApp
+//
+//  Created by taehyeon.lee on 2022/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
