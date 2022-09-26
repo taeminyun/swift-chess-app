2d074deb1835452dc3a0cb999480aa26fc3288eb
Dongchan Son <sonfrdy@gmail.com>
2022-09-26 13:21:39 +0900

    프로젝트 생성

---
+++ SwiftChessApp/SwiftChessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftChessApp
+//
+//  Created by Freddy on 2022/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+  
+}
+
