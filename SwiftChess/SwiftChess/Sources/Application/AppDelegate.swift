b4b01bb6eb090e6b333d43e63bca67e146dd256c
Geonhee Son <ryan.son1002@gmail.com>
2022-09-26 21:50:16 +0900

    style: 프로젝트 폴더 구조 변경 및 불필요 주석 제거

---
+++ SwiftChess/SwiftChess/Sources/Application/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  SwiftChess
+//
+//  Created by Geonhee on 2022/09/26.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        return UISceneConfiguration(
+            name: "Default Configuration",
+            sessionRole: connectingSceneSession.role
+        )
+    }
+}
+
--- SwiftChess/SwiftChess/Sources/Home/HomeViewController.swift
+++ SwiftChess/SwiftChess/Sources/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  SwiftChess
 //
 //  Created by Geonhee on 2022/09/26.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
