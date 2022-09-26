dda86338509f01301d0b3388e218cb7485b8a28f
Beepeach <eh7457303j@gmail.com>
2022-09-26 18:33:10 +0900

    Add: ChessGame 프로젝트 생성

---
+++ ChessGame/ChessGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessGame
+//
+//  Created by JunHeeJo on 9/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
