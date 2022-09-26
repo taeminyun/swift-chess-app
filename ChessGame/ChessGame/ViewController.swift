62a1d72bc178957442f7036cf83911d9f7370632
delmaSong <sdy2856@gmail.com>
2022-09-26 21:20:53 +0900

    chore: 프로젝트 생성

---
+++ ChessGame/ChessGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessGame
+//
+//  Created by Delma Song on 2022/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
