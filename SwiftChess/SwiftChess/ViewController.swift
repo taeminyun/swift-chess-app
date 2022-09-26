978cdf43c96172f7cf545bbdfe109071bd35385a
Geonhee Son <ryan.son1002@gmail.com>
2022-09-26 21:34:41 +0900

    feat: 프로젝트 추가

---
+++ SwiftChess/SwiftChess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftChess
+//
+//  Created by Geonhee on 2022/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
