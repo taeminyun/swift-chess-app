9d589f389a8c847bb45d08f88084a7be2a7aa37f
임승혁 <seunghyeok.lim@kurlycorp.com>
2022-09-26 22:17:33 +0900

    feature: 프로젝트 생성

---
+++ ChessApp/ChessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessApp
+//
+//  Created by MK-AM16-010 on 2022/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
