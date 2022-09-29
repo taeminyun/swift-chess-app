8277e2b43810da72c9e9286f416bd0551ef8a45f
taeminyun <taemin.yun@wesang.com>
2022-09-30 08:02:09 +0900

    create project
    - Interface: Storyboard
    - Include Tests

---
+++ swift chess app/swift chess app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift chess app
+//
+//  Created by 윤태민(Yun, Taemin) on 2022/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ swift chess app/swift chess appUITests/swift_chess_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  swift_chess_appUITestsLaunchTests.swift
+//  swift chess appUITests
+//
+//  Created by 윤태민(Yun, Taemin) on 2022/09/27.
+//
+
+import XCTest
+
+final class swift_chess_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
